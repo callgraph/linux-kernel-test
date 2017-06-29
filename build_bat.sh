962cb5c5da403a15e59c113d078673897e32cd25
mao_ying_ming <elwin_mao@163.com>
2017-06-29 19:03:50 +0800

    Update build_bat.sh

--- build_bat.sh
+++ build_bat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-./rt-linux-lkp.sh    https://github.com/chyyuu/linux-rt-devel.git  v4.11.5-rt1
-./rt-linux-lkp.sh    https://github.com/chyyuu/linux-rt-devel.git  v4.9-rt1
-./rt-linux-lkp.sh    https://github.com/chyyuu/linux-rt-devel.git  v4.8-rt1
-./rt-linux-lkp.sh    https://github.com/chyyuu/linux-rt-devel.git  v4.6.1-rt2
-./rt-linux-lkp.sh    https://github.com/chyyuu/linux-rt-devel.git  v4.4-rt2
-./rt-linux-lkp.sh    https://github.com/chyyuu/linux-rt-devel.git  v4.1.2-rt1
+./rt-linux-lkp.sh    https://github.com/torvalds/linux.git  v4.11
+./rt-linux-lkp.sh    https://github.com/torvalds/linux.git  v4.9
+./rt-linux-lkp.sh    https://github.com/torvalds/linux.git  v4.8
+./rt-linux-lkp.sh    https://github.com/torvalds/linux.git  v4.6
+./rt-linux-lkp.sh    https://github.com/torvalds/linux.git  v4.4
+./rt-linux-lkp.sh    https://github.com/torvalds/linux.git  v4.1
